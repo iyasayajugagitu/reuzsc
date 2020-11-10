4e7bcf5d3ddadd1fcdd69219dad409f40874d48b
benisadewa <73853490+benisadewa@users.noreply.github.com>
2020-11-10 23:02:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -175,6 +175,25 @@ var idn = num.replace("0","+62");
 console.log(id);
 const gg = idn
 
+case '!stickergif':
+        case '!stikergif':
+        case '!sgif':
+            if (isMedia) {
+                if (mimetype === 'video/mp4' && message.duration < 10 || mimetype === 'image/gif' && message.duration < 10) {
+                    const mediaData = await decryptMedia(message, uaOverride)
+                    client.reply(from, '[WAIT] Sedang di proses⏳ silahkan tunggu ± 1 min!', id)
+                    const filename = `./media/aswu.${mimetype.split('/')[1]}`
+                    await fs.writeFileSync(filename, mediaData)
+                    await exec(`gify ${filename} ./media/output.gif --fps=30 --scale=240:240`, async function (error, stdout, stderr) {
+                        const gif = await fs.readFileSync('./media/output.gif', { encoding: "base64" })
+                        await client.sendImageAsSticker(from, `data:image/gif;base64,${gif.toString('base64')}`)
+                    })
+                } else (
+                    client.reply(from, '[!] Kirim video dengan caption *!stickerGif* max 10 sec!', id)
+                )
+            }
+            break
+	
 const exists = await conn.isOnWhatsApp (gg)
 console.log(exists);
 conn.sendMessage(id ,`nomor hp ${gg} ${exists ? " tersedia " : " tidak tersedia"} di whatsapp`, MessageType.text)
